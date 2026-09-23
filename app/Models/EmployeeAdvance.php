3a457b0b621bba2c069835f24cd32dc0eee1d47b
LOAI W . A MIQDAD <121128678+hicatssam@users.noreply.github.com>
2026-09-23 19:05:33 +0300

    Relate employee advances to repayments

--- app/Models/EmployeeAdvance.php
+++ app/Models/EmployeeAdvance.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class EmployeeAdvance extends Model
 {
@@ -36,4 +37,13 @@ class EmployeeAdvance extends Model
     {
         return $this->belongsTo(Employee::class);
     }
+
+    public function repayments(): HasMany
+    {
+        return $this->hasMany(
+            EmployeeAdvanceRepayment::class,
+            'employee_advance_id'
+        )->latest('paid_at')->latest('id');
+    }
+
 }
